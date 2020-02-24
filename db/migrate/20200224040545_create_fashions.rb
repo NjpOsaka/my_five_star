7193e363f628a082b255139dc6fede45b05c242e
NjpOsaka <n.jposaka@gmail.com>
2020-02-24 13:22:08 +0900

    deviseinstall

---
+++ db/migrate/20200224040545_create_fashions.rb
@@ -0,0 +1,11 @@
+class CreateFashions < ActiveRecord::Migration[5.0]
+  def change
+    create_table :fashions do |t|
+      t.binary :image
+      t.string :title
+      t.text :story
+      t.string :url
+      t.timestamps
+    end
+  end
+end
