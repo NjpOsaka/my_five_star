455139e86bd29671a47b8e0176d4a0620c90034c
NjpOsaka <n.jposaka@gmail.com>
2020-02-26 12:30:04 +0900

    showpage

--- app/controllers/fashions_controller.rb
+++ app/controllers/fashions_controller.rb
@@ -1,14 +1,33 @@
 class FashionsController < ApplicationController
+
   def index
-    
+    @fashions = Fashion.all
   end
 
   def new
-  
+   @fashion = Fashion.new
   end
 
   def show
-    
+    @fashion = Fashion.find(params[:id])
   end
 
+  def create
+    @fashion = Fashion.new(fashion_params)
+    @fashion.save
+    redirect_to root_path
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  
+
+  def fashion_params
+    params.require(:fashion).permit(:title, :story, :url)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root "users#index"
-  resources :fashions, only: [:index, :new, :show]
+  resources :fashions, only: [:index, :new, :show, :create]
 end
